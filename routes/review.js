a6a37490e698dfb844ab49bfa784956389fd7538
Deen Bandhu Singh <deenbandhusingh335@gmail.com>
2023-11-28 14:25:50 +0530

    Initial commit

---
+++ routes/review.js
@@ -0,0 +1,30 @@
+const  express=require("express");
+const router= express.Router( {mergeParams: true});
+const wrapAsync=require("../utils/wrapAsync.js");
+const ExpressError=require("../utils/ExpressError.js");
+
+const Listing = require("../models/listing.js");
+const Review = require("../models/review.js"); // Add this line
+
+const {validateReview, isLoggedIn, isreviewauthor}=require("../middleware.js");
+
+const reviewController=require("../controllers/reviews.js")
+
+
+//Post Review Route
+router.post("/",
+isLoggedIn,
+validateReview, 
+wrapAsync(reviewController.createReview)
+);
+
+//Delete Review Route
+router.delete("/:reviewId",
+isLoggedIn,
+isreviewauthor
+,
+wrapAsync(reviewController.destroyReview));
+
+
+
+module.exports=router;
